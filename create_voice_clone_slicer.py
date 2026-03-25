491d5c5359ffa2bbdead42cf38c28fa8e0e38e55
jdc4429 <jdc4429@gmail.com>
2026-03-24 23:18:39 -0400

    Update create_voice_clone_slicer.py

--- create_voice_clone_slicer.py
+++ create_voice_clone_slicer.py
@@ -3,15 +3,12 @@
 # Run this script after editing the text_content = '<Whatever you want to say>'
 # Then run the generated generated_tts.py: python generated_tts.py
 # When it's done running, you will see the file named output_voice_clone.wav
-# If you have multiple files, use _1, _2, etc after each file to be included.  ie. Train_1.wav, Train_2.wav
-# You can edit the variable below; training_audio = ' ' to whatever you want the base filename of your audio files to be.
 
 import re
 import os
 import numpy as np
 import soundfile as sf
 from scipy import signal
-training_audio = 'Train'
 pause_duration = 0.5
 output_audio = "combined_reference.wav"
 output_text = "combined_reference.txt"
